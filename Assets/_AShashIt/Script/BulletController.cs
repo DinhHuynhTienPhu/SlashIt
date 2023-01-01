abcfb7b83f895d443bab5180a88dab4463162e8b
DinhHuynhTienPhu <ff10111011@gmail.com>
2023-01-01 19:24:02 +0700

    Update BulletController.cs

--- Assets/_AShashIt/Script/BulletController.cs
+++ Assets/_AShashIt/Script/BulletController.cs
@@ -8,7 +8,7 @@ public class BulletController : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-
+      //do notthoing here
   }
 
   // Update is called once per frame
